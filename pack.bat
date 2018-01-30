35706998eaf87c09e9352b29873da9ef597ddc67
Roger Viñas Alcon <roger.vinas@scmspain.com>
2018-01-30 17:50:41 +0100

    Change assembly info

--- pack.bat
+++ pack.bat
@@ -7,12 +7,8 @@ SET CACHED_NUGET=%LocalAppData%\NuGet\NuGet.exe
 
 cd src\Zipkin.Core
 
-SET TITLE="Zipkin Core"
-SET VERSION=1.0
-SET AUTHOR=openzipkin
-SET DESCRIPTION="A minimalistic .NET client library for Twitter Zipkin tracing."
-SET SUMMARY="Build from fork https://github.com/rogervinas/zipkin-csharp"
+SET SUMMARY=Zipkin.Core forked to https://github.com/rogervinas/zipkin-csharp
 
-%CACHED_NUGET% pack Zipkin.Core.csproj -IncludeReferencedProjects -Properties Configuration=Release;title=%TITLE%;version=%VERSION%;author=%AUTHOR%;description=%DESCRIPTION%;summary=%SUMMARY% -OutputDirectory ..\..
+%CACHED_NUGET% pack Zipkin.Core.csproj -OutputDirectory ..\.. -IncludeReferencedProjects -Properties Configuration=Release
 
 popd
